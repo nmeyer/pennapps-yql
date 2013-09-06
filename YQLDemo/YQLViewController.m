4fdb8af95f04712dcf55e3baf0183a614cf8b083
Pierre Maloka <pmaloka@yahoo-inc.com>
2013-09-06 11:28:20 -0400

    Add sample code.

---
+++ YQLDemo/YQLViewController.m
@@ -0,0 +1,56 @@
+//
+//  YQLViewController.m
+//  YQLDemo
+//
+//  Created by Pierre Maloka on 9/6/13.
+//  Modified by Nick Meyer on 9/6/13.
+//
+
+#import "YQLViewController.h"
+
+@interface YQLViewController ()
+
+@end
+
+@implementation YQLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    NSString *yqlQuery = @"http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20craigslist.search%20where%20location%3D%22newyork%22%20and%20type%3D%22sss%22%20and%20query%3D%22mustang%20convertable%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
+    
+    NSDictionary *results = [self downloadJSON:yqlQuery];
+    
+    NSLog(@"Got JSON:");
+    NSLog(@"%@", results);
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+- (NSDictionary *)downloadJSON:(NSString *)query
+{
+    NSError *error;
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:query]];
+    NSData *response = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:&error];
+    
+    if (!response || error) {
+        NSLog(@"%@", error.description);
+        return nil;
+    }
+    
+    NSDictionary *jsonDictionary = [NSJSONSerialization JSONObjectWithData:response options:kNilOptions error:&error];
+    
+    if (error) {
+        NSLog(@"%@", error.description);
+        return nil;
+    }
+    
+    return jsonDictionary;
+}
+
+@end
